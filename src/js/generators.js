11d9cd9966b8113e36d1fafa98f233d664d60258
wee-owl <kem-cv@yandex.ru>
2023-06-20 19:42:27 +0300

    feat: generate teams

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-/* eslint-disable no-unused-vars */
 /**
  * Формирует экземпляр персонажа из массива allowedTypes со
  * случайным уровнем от 1 до maxLevel
@@ -10,8 +8,21 @@
  * возвращает новый экземпляр класса персонажа
  *
  */
+import Team from './Team';
+
 export function* characterGenerator(allowedTypes, maxLevel) {
-  // TODO: write logic here
+  function randomInteger(min, max) {
+    const num = min + Math.random() * (max + 1 - min);
+    return Math.floor(num);
+  }
+  let index;
+  let lvl;
+
+  while (true) {
+    index = randomInteger(0, allowedTypes.length);
+    lvl = randomInteger(1, maxLevel);
+    yield new allowedTypes[index](lvl);
+  }
 }
 
 /**
@@ -19,8 +30,21 @@ export function* characterGenerator(allowedTypes, maxLevel) {
  * @param allowedTypes массив классов
  * @param maxLevel максимальный возможный уровень персонажа
  * @param characterCount количество персонажей, которое нужно сформировать
- * @returns экземпляр Team, хранящий экземпляры персонажей. Количество персонажей в команде - characterCount
+ * @returns экземпляр Team, хранящий экземпляры персонажей.
+ * Количество персонажей в команде - characterCount
  * */
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
-  // TODO: write logic here
+  if (!Array.isArray(allowedTypes)) {
+    throw new Error('Available player classes must be passed as an array');
+  }
+
+  const playersArray = [];
+
+  for (let i = 1; i <= characterCount; i += 1) {
+    const generateCharacter = characterGenerator(allowedTypes, maxLevel);
+    const newCharacter = generateCharacter.next().value;
+    playersArray.push(newCharacter);
+  }
+
+  return new Team(playersArray);
 }
