b94e0de379b94c0fdde9127cc734a8ed25ff95ee
wee-owl <kem-cv@yandex.ru>
2023-06-21 15:48:53 +0300

    feat: add team characters to the field

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,6 +1,13 @@
-/* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-vars */
-import GamePlay from './GamePlay';
+/* eslint-disable class-methods-use-this */
+import PositionedCharacter from './PositionedCharacter';
+import Swordsman from './characters/Swordsman';
+import Magician from './characters/Magician';
+import { generateTeam } from './generators';
+import Vampire from './characters/Vampire';
+import Bowman from './characters/Bowman';
+import Daemon from './characters/Daemon';
+import Undead from './characters/Undead';
 import themes from './themes';
 
 export default class GameController {
@@ -10,20 +17,92 @@ export default class GameController {
   }
 
   init() {
-    this.gamePlay.drawUi(themes.lvl1);
-    // TODO: add event listeners to gamePlay events
-    // TODO: load saved stated from stateService
+    this.renderField(1);
+
+    const playerChar = [Bowman, Swordsman, Magician];
+    const enemyChar = [Daemon, Undead, Vampire];
+
+    const playerTeam = this.getPositionedCharacter(this.renderTeam(playerChar), this.renderPosition('player'));
+    const enemyTeam = this.getPositionedCharacter(this.renderTeam(enemyChar), this.renderPosition('enemy'));
+    this.gamePlay.redrawPositions(playerTeam.concat(enemyTeam));
+
+    // add event listeners to gamePlay events
+    // load saved stated from stateService
+  }
+
+  renderField(level) {
+    let area;
+    if (level === 1) area = themes.lvl1;
+    if (level === 2) area = themes.lvl2;
+    if (level === 3) area = themes.lvl3;
+    if (level === 4) area = themes.lvl4;
+    return this.gamePlay.drawUi(area);
+  }
+
+  renderTeam(array) {
+    const maxLevel = 4;
+    const characterCount = 3;
+    const team = generateTeam(array, maxLevel, characterCount);
+    return team.characters;
+  }
+
+  renderPosition(typeTeam) {
+    const boardSize = 8;
+    let positionArray = [];
+    positionArray.length = 3;
+
+    // array for players character (column 1-2)
+    const playerPositionArray = [...Array(boardSize ** 2)].map((_, i) => i)
+      .filter((i) => i % boardSize === 0 || (i - 1) % boardSize === 0);
+
+    // array for players character (column 7-8)
+    const enemyPositionArray = [...Array(boardSize ** 2)].map((_, i) => i)
+      .filter((i) => (i + 1) % boardSize === 0 || (i + 2) % boardSize === 0);
+
+    // render random index from array
+    function renderIndex(array) {
+      return Math.floor(Math.random() * (array.length - 1));
+    }
+
+    // check array for unique el
+    function renderArray(array) {
+      const set = new Set(array);
+      return set.size === positionArray.length;
+    }
+
+    function renderPositionArray(array) {
+      while (!renderArray(positionArray)) {
+        positionArray = [
+          array[renderIndex(array)],
+          array[renderIndex(array)],
+          array[renderIndex(array)],
+        ];
+      }
+      return positionArray;
+    }
+
+    return typeTeam === 'player'
+      ? renderPositionArray(playerPositionArray)
+      : renderPositionArray(enemyPositionArray);
+  }
+
+  getPositionedCharacter(team, positionArray) {
+    const completeTeam = [];
+    for (let i = 0; i < team.length; i += 1) {
+      completeTeam.push(new PositionedCharacter(team[i], positionArray[i]));
+    }
+    return completeTeam;
   }
 
   onCellClick(index) {
-    // TODO: react to click
+    // react to click
   }
 
   onCellEnter(index) {
-    // TODO: react to mouse enter
+    // react to mouse enter
   }
 
   onCellLeave(index) {
-    // TODO: react to mouse leave
+    // react to mouse leave
   }
 }
--- src/js/PositionedCharacter.js
+++ src/js/PositionedCharacter.js
@@ -3,11 +3,11 @@ import Character from './Character';
 export default class PositionedCharacter {
   constructor(character, position) {
     if (!(character instanceof Character)) {
-      throw new Error('character must be instance of Character or its children');
+      throw new Error('Character must be instance of Character or its children');
     }
 
     if (typeof position !== 'number') {
-      throw new Error('position must be a number');
+      throw new Error('Position must be a number');
     }
 
     this.character = character;
